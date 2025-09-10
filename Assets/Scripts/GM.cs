5eda0867665ef64571028d9cd739780b541f09c9
Jasonlin778 <linlinlinlinlin500@gmail.com>
2025-09-10 21:02:32 +0800

    20250910 Jason

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -23,13 +23,31 @@ public class GM : MonoBehaviour
 
     // 宣告一個資料型別為GameObject(遊戲物件)的變數，命名為mahjong
     public GameObject majhong;
+    public Vector3 secret = new Vector3(60.85f, 72.4f, 83.7f);
+    public Transform mountain;
 
     // Start is called before the first frame update
     // start這個功能會在update的第一幀的畫面前被呼叫執行，也就是一開始會執行一次
     // Start()這個功能是由UnityEngine這個函式庫提供的，因此沒有寫"using UnityEngine"的話就不會在第一幀的畫面前被呼叫執行
     void Start()
     {
+        
+        // Debug.Log("字串")：輸出指定的訊息至Unity的Console主控台中，可以用來除錯使用
+        //這個功能可以傳一個字串進去
+        //字串可以做加法，沒有減法
+        Debug.Log("執行start");
+    }
 
+    // Update is called once per frame
+    // update這個功能每一幀的畫面會被呼叫執行一次，也就是一秒大概執行大約60次
+    void Update()
+    {
+        Debug.Log("執行update");
+    }
+
+    public void StartGame()
+    {
+        // function函式小括號內的東西叫做parameters參數
         // Instantiate(遊戲物件)：是一個函式，給他一個遊戲物件(GameObject)後，能生成一個遊戲物件
         // Instantiate(遊戲物件, 向量, 角度)：除了生成遊戲物件外，還能指定他的座標與旋轉角度
         // Instantiate(majhong);
@@ -63,40 +81,32 @@ public class GM : MonoBehaviour
 
         // i++的意思：i加1
 
+        // 把secret變數的z值輸出
+        Debug.Log(secret.z);
 
-        for (int i = 0; i < 17; i++) 
+        for (int i = 0; i < 17; i++)
         {
             // Instantiate(遊戲物件, 向量, 角度)：複製生成，除了生成遊戲物件外，還能指定他的座標與旋轉角度
+            // 向量 Vector3：是一種資料型別，可以儲存3個float，依序為x,y,z
 
             //自己
-            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.95f, -3.8f), Quaternion.Euler(270, 0, 0));
-            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.75f, -3.8f), Quaternion.Euler(270, 0, 0));
+            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.95f, -2.8f), Quaternion.Euler(270, 0, 0), mountain);
+            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.75f, -2.8f), Quaternion.Euler(270, 0, 0), mountain);
 
             //上家
-            Instantiate(majhong, new Vector3(-3.8f, -0.95f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0));
-            Instantiate(majhong, new Vector3(-3.8f, -0.75f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0));
+            Instantiate(majhong, new Vector3(-2.8f, -0.95f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0), mountain);
+            Instantiate(majhong, new Vector3(-2.8f, -0.75f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0), mountain);
 
             //對家
-            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.95f, 3.8f), Quaternion.Euler(270, 0, 0));
-            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.75f, 3.8f), Quaternion.Euler(270, 0, 0));
+            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.95f, 2.8f), Quaternion.Euler(270, 0, 0), mountain);
+            Instantiate(majhong, new Vector3(-2.4f + (i * 0.3f), -0.75f, 2.8f), Quaternion.Euler(270, 0, 0), mountain);
 
             //下家
-            Instantiate(majhong, new Vector3(3.8f, -0.95f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0));
-            Instantiate(majhong, new Vector3(3.8f, -0.75f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0));
+            Instantiate(majhong, new Vector3(2.8f, -0.95f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0), mountain);
+            Instantiate(majhong, new Vector3(2.8f, -0.75f, -2.4f + (i * 0.3f)), Quaternion.Euler(270, 0, 0), mountain);
             Debug.Log("第" + i + "次迴圈");
         }
-
-        // Debug.Log("字串")：輸出指定的訊息至Unity的Console主控台中，可以用來除錯使用
-        //這個功能可以傳一個字串進去
-        //字串可以做加法，沒有減法
-        Debug.Log("執行start");
-    }
-
-    // Update is called once per frame
-    // update這個功能每一幀的畫面會被呼叫執行一次，也就是一秒大概執行大約60次
-    void Update()
-    {
-        Debug.Log("執行update");
+        mountain.rotation = Quaternion.Euler(0, 20, 0);
     }
 }
 
@@ -121,6 +131,9 @@ public class [物品名稱] : MonoBehavior
         // 2. int：整數(原本的英文為integer 存12345678 ... )
         // 3. string：字串，可以儲存一段文字，可以是中文 (記得文字外面要用引號""夾住，例如 "Hello world")
         // 4. float：浮點數，小數 (記得數值後面要加f表示他是小數，例如、4是整數，4f、4.0f、4.222f是小數)
+        // 5. Vector3：向量，可以儲存3個小數，分別為x,y,z
+        // 6. Transform：一個物件在空間中的資訊，包含座標、旋轉角度、尺寸大小、親parent子child物件關係
+                                                  父物件：parent object，子物件：child object
     // 變數的命名規則：
         // 1. 第一個字只能是大小寫英文A-Z a-z或是底線_
         // 2. 第二個字以後只能是大小寫英文A-Z a-z、數字0-9、底線_
@@ -133,7 +146,7 @@ public class [物品名稱] : MonoBehavior
         return [回傳值，若無則不填];
     }
     // 你要知道的：
-    void [函數名字](){
+    public void [函數名字](){
         
     }
     // void：空白、無東西的
@@ -150,6 +163,7 @@ public class [物品名稱] : MonoBehavior
     身高：179.9
     年齡：20
     學生：Eason
+    三圍：60.85 , 72.4 , 83.7
     一個物品有很多功能，也有資料可以儲存
 
     // 把上述資料轉換成變數儲存
@@ -157,7 +171,8 @@ public class [物品名稱] : MonoBehavior
     public float height = 179.9f;
     public int age = 20;
     public string student = "Eason";
-
+    public Vector3 secret = new Vector3( 60.85f, 72.4f , 83.7f);
+    // new Vector3( 60.85f, 72.4f , 83.7f)代表建立一個新的向量物件儲存於變數secret中
     // 老師有兩個功能可以做，第一個：催促作業；第二個：計算學費
     // 函數命名習慣，每個單字的第一個字為大寫
     // 程式的寫法
